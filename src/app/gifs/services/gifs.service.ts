776adb4a6580589ce5fc359d78d50f9363c739dd
andres.mera <feli041089@gmail.com>
2024-02-17 08:38:27 -0500

    dev : Validaciones al servicio.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,16 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  public searchTag(tag: string): void {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+    if (this._tagHistory.includes(tag)) { this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag); }
     this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0, 10);
+  }
+
+
+  public searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
   }
 }
