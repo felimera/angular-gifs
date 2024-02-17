f5eda854eddcef97a9887a1323b876d7f8602b5e
andres.mera <feli041089@gmail.com>
2024-02-17 10:20:25 -0500

    dev : Realizar una peticion Http.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
@@ -5,8 +6,9 @@ export class GifsService {
 
   private _tagHistory: string[] = [];
   private apiKey: string = 'OaMUIcD9zZ01fo7QmXvriW6RywmShWTQ';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagHistory(): string[] {
     return [...this._tagHistory];
@@ -20,8 +22,26 @@ export class GifsService {
   }
 
 
-  public searchTag(tag: string): void {
+  searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=OaMUIcD9zZ01fo7QmXvriW6RywmShWTQ&q=Valorant&limit=10')
+    // .then(resp=>resp.json())
+    // .then(data=>console.log(data));
+    
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', '10');
+
+    this.http
+      .get(`${this.serviceUrl}/search`, { params })
+      .subscribe({
+        next: (resp) => {
+          console.log(resp)
+        },
+        error: (resp) => console.log(resp.error)
+      })
   }
 }
